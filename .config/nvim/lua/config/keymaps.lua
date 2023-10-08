52ded66dba843325955be782c10d1620a66e701b
Askath <taradruffel@outlook.com>
2023-10-08 17:20:19 +0200

    init

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.del("n", "<leader>gg")
+vim.keymap.del("n", "<leader>gG")
+vim.keymap.del("n", "<leader><tab>l")
+vim.keymap.del("n", "<leader><tab><tab>")
+vim.keymap.del("n", "<leader><tab>]")
+vim.keymap.del("n", "<leader><tab>d")
+vim.keymap.del("n", "<leader><tab>[")
+vim.keymap.del("n", "<leader><tab>f")
+
+vim.keymap.set("n", "<leader>W", "<cmd>w!<CR>", { desc = "Save" })
---
+++ .zshrc
@@ -0,0 +1,9 @@
+
+export PATH=/Users/taradruffel/nvim/bin:$PATH
+
+eval "$(starship init zsh)"
+eval "$(zoxide init zsh)"
+
+alias ls="exa -al"
+alias cat=bat
+alias n=nvim
