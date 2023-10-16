b2ad0facf73cdc40ebe6c6cab9298debba211d7e
Askath <taradruffel@outlook.com>
2023-10-16 09:16:14 +0200

    config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,13 @@ return {
         angularls = {
           root_dir = require("lspconfig.util").root_pattern("angular.json", "project.json"),
         },
+        elixirls = {
+          dialyzerEnabled = true,
+          autoBuild = true,
+          cmd = { "elixir-ls" },
+          filetypes = { "elixir", "eelixir", "exs", "ex" },
+          root_dir = require("lspconfig.util").root_pattern("mix.exs", ".git"),
+        },
       },
     },
   },
--- .zshrc
+++ .zshrc
@@ -18,3 +18,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+. /opt/homebrew/opt/asdf/libexec/asdf.sh
