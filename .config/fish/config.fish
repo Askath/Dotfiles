3f9c7aa5fe7ce9adde2a9f2686215c85d609b44b
Askath <taradruffel@outlook.com>
2023-11-15 18:11:17 +0100

    added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+set PATH /Users/taradruffel/nvim/bin $PATH
+
+set PATH /opt/homebrew/opt/openjdk@17/bin $PATH
+set -x JAVA_HOME /opt/homebrew/opt/openjdk@17
+set -x USERNAME taradruffel
+
+starship init fish | source
+zoxide init fish | source
+
+alias ls="exa -al"
+alias cat='bat'
+alias n='nvim'
