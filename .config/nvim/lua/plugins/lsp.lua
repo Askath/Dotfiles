eafeb2386f50391e31355e94c989b861bc438f8e
Tara Druffel <t.druffel@outlook.com>
2023-11-15 15:08:50 +0100

    test

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,66 +1,93 @@
 return {
-
-  {
-
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        angularls = {
-          file_types = { "typescript", "typescriptreact", "html", "typescript.tsx" },
-          root_dir = require("lspconfig.util").root_pattern("angular.json", "project.json"),
+    { "lvimuser/lsp-inlayhints.nvim" },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                angularls = {
+                    file_types = {
+                        "typescript",
+                        "typescriptreact",
+                        "html",
+                        "typescript.tsx",
+                    },
+                    root_dir = require("lspconfig.util").root_pattern(
+                        "angular.json",
+                        "project.json"
+                    ),
+                },
+                elixirls = {
+                    dialyzerEnabled = true,
+                    autoBuild = true,
+                    cmd = { "elixir-ls" },
+                    filetypes = { "elixir", "eelixir", "exs", "ex" },
+                    root_dir = require("lspconfig.util").root_pattern(
+                        "mix.exs",
+                        ".git"
+                    ),
+                },
+                gdscript = {
+                    file_types = { "gd", "gdscript" },
+                    root_dir = require("lspconfig.util").root_pattern(
+                        "project.godot",
+                        ".git"
+                    ),
+                },
+            },
         },
-        elixirls = {
-          dialyzerEnabled = true,
-          autoBuild = true,
-          cmd = { "elixir-ls" },
-          filetypes = { "elixir", "eelixir", "exs", "ex" },
-          root_dir = require("lspconfig.util").root_pattern("mix.exs", ".git"),
-        },
-        gdscript = {
-          file_types = { "gd", "gdscript" },
-          root_dir = require("lspconfig.util").root_pattern("project.godot", ".git"),
-        },
-      },
     },
-  },
+    { "habamax/vim-godot" },
+    {
+        "elixir-tools/elixir-tools.nvim",
+        version = "*",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local elixir = require("elixir")
+            local elixirls = require("elixir.elixirls")
 
-  { "habamax/vim-godot" },
-  {
-    "elixir-tools/elixir-tools.nvim",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
+            elixir.setup({
+                nextls = {
+                    enable = true, -- defaults to false
+                },
+                credo = {
+                    enable = true, -- defaults to true
+                },
+                elixirls = {
+                    -- specify a repository and branch
+                    -- repo = "mhanberg/elixir-ls", -- defaults to elixir-lsp/elixir-ls
 
-      elixir.setup({
-        nextls = {
-          enable = true, -- defaults to false
+                    -- default settings, use the `settings` function to override settings
+                    settings = elixirls.settings({
+                        dialyzerEnabled = true,
+                        fetchDeps = false,
+                        enableTestLenses = false,
+                        suggestSpecs = false,
+                    }),
+                    on_attach = function(client, bufnr)
+                        vim.keymap.set(
+                            "n",
+                            "<space>fp",
+                            ":ElixirFromPipe<cr>",
+                            { buffer = true, noremap = true }
+                        )
+                        vim.keymap.set(
+                            "n",
+                            "<space>tp",
+                            ":ElixirToPipe<cr>",
+                            { buffer = true, noremap = true }
+                        )
+                        vim.keymap.set(
+                            "v",
+                            "<space>em",
+                            ":ElixirExpandMacro<cr>",
+                            { buffer = true, noremap = true }
+                        )
+                    end,
+                },
+            })
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
         },
-        credo = {
-          enable = true, -- defaults to true
-        },
-        elixirls = {
-          -- specify a repository and branch
-          -- repo = "mhanberg/elixir-ls", -- defaults to elixir-lsp/elixir-ls
-
-          -- default settings, use the `settings` function to override settings
-          settings = elixirls.settings({
-            dialyzerEnabled = true,
-            fetchDeps = false,
-            enableTestLenses = false,
-            suggestSpecs = false,
-          }),
-          on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-          end,
-        },
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
     },
-  },
 }
