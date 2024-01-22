4dc343050ef858c3d89438c819ee2e8368399e1c
Askath <taradruffel@outlook.com>
2024-01-22 22:11:25 +0100

    edit?

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -7,6 +7,11 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
+
+    {
+        "codota/tabnine-nvim",
+        build = "./dl_binaries.sh",
+    },
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
@@ -41,4 +46,14 @@ require("lazy").setup({
             },
         },
     },
+
+    require("tabnine").setup({
+        disable_auto_comment = true,
+        accept_keymap = "<M-l>",
+        dismiss_keymap = "<C-]>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+        log_file_path = "/Users/taradruffel/test.log", -- absolute path to Tabnine log file
+    }),
 })
